5d9762118401d1000461126a1678f7e419ed6553
Jeta Fazliu <jfazliu07@gmail.com>
2023-12-12 14:56:47 -0800

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark");
+
     var logo = document.querySelector('.dode');
     logo.classList.toggle('dode-dark');
+
+    var textElements = document.querySelectorAll('.text-light');
+    textElements.forEach(function(element) {
+        element.classList.toggle('text-dark');
+    });
+
     var rrejte = document.querySelector(".ic");
     rrejte.classList.toggle('icd');
 }
+
+
+
